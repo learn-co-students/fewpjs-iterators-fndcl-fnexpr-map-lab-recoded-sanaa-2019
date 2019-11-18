adf2bf62f124f2ce18abd1d30cb92175d6ad1b14
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-11-18 13:00:12 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let tutorials2 = tutorials.map(tutorial=>{
+    let newTutorials = tutorial.split(" ");
+  let newString="";
+  for(let i=0;i<newTutorials.length;i++)
+  {
+    newTutorials[i]=newTutorials[i][0].toUpperCase()+newTutorials[i].slice(1)+" ";
+    newString+=newTutorials[i];
+  }
+  return newString.slice(0,newString.length-1);
+  }
+  )
+  
+  return tutorials2
 }
